2c01a999fa561df6f91a5b0564e869e41400b628
Francis <hisconer@gmail.com>
2021-01-09 10:46:32 +0100

    Add postcss-combine-media-query to merge duplicate mediaqueries

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   plugins: {
-    "postcss-preset-env": {
-      stage: 0,
+    "postcss-preset-env": { stage: 0 },
+    "postcss-combine-media-query": {},
+    cssnano: {
+      preset: ["advanced", { discardComments: { removeAll: false } }],
     },
-    cssnano: {},
   },
 };
 
