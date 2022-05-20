a1b0d9ca6ddb4b3017fcec8523a222eb0f234370
FrancisVega <hisconer@gmail.com>
2022-05-20 10:45:07 +0200

    Work on sass

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   plugins: {
     "postcss-preset-env": { stage: 0 },
-    "postcss-combine-media-query": {},
+    "postcss-sort-media-queries": {},
+    "postcss-combine-duplicated-selectors": {
+      removeDuplicatedProperties: true,
+    },
     cssnano: {
       preset: ["advanced", { discardComments: { removeAll: false } }],
     },
   },
 };
-
